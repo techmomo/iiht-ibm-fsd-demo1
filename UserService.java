dd2342bed05f0974b0271d7c32c29946b7004c0b
Techmomo <momo@tech.edu>
2021-03-12 07:02:10 +0000

    set name

--- UserService.java
+++ UserService.java
@@ -1,9 +1,10 @@
 class UserService{
 
-  public static String getUser(int id){
+  public static String getUser(int id){	  
      return "User";
   }
    public static void setUser(User u){
      User u = new User();
+     u.setName(u.getId() + "--");
   }
 }
